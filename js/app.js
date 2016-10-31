c1442fbfb8ea89f1157b41d104529ac43663751b
John McCain <johnm.freestate@gmail.com>
2016-10-30 19:18:50 -0500

    Refactor url processing

--- js/app.js
+++ js/app.js
@@ -3,14 +3,42 @@ require('../style/style.scss');
 var app = angular.module('fbtogc', []);
 
 app.controller('myCtrl', function($scope) {
-    $scope.eventURLS = [''];
-    $scope.removeURL = function(index) {
-        $scope.eventURLS.splice(index, 1);
-    };
-    $scope.addURL = function() {
-        $scope.eventURLS.push('');
-    };
+	$scope.eventUrls = [{
+		val: ''
+	}];
+	$scope.removeURL = function(index) {
+		$scope.eventUrls.splice(index, 1);
+	};
+	$scope.addURL = function() {
+		console.log($scope.eventUrls);
+		$scope.eventUrls.push({
+			val: ''
+		});
+		console.log($scope.eventUrls);
+
+	};
+	$scope.processUrls = function() {
+		console.log('processing urls');
+		let eventIds = [];
+		for (let i = 0; i < $scope.eventUrls.length; ++i) {
+			let startIndex = $scope.eventUrls[i].val.indexOf("/events/");
+			if ($scope.eventUrls[i].val && startIndex > 0) {
+				//TODO: refactor this
+				if ($scope.eventUrls[i].val.charAt($scope.eventUrls[i].val.length - 1) != '/') {
+					eventIds.push($scope.eventUrls[i].val.substring(startIndex + 8, $scope.eventUrls[i].val.length - 1));
+				}
+			} else {
+				//TODO: add form feedback
+				console.log('Error: url ' + (i + 1) + ' is not a facebook event. ' + $scope.eventUrls[i].val);
+			}
+		}
+
+		//TODO: process events
+	};
+	$scope.resetUrls = function() {
+		$scope.eventUrls = [''];
+	};
 	$scope.hello = function() {
-		alert('hello');
+		console.log($scope.eventUrls);
 	};
 });
--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
 			}
 		}, {
 			test: /\.scss$/,
-			loader: "style-loader!css-loader!sass-loader"
+			loader: "style-loader!css-loader!autoprefixer-loader!sass-loader"
 		}]
 	},
 	output: {
